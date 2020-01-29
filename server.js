02aa05db5c2a19805349e2d1ac292fb9d39c3a70
AntoniaCatalinaCarasel <caraselcatalina17@stud.ase.ro>
2020-01-29 20:35:13 +0000

    Seminar

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ const app = express()
  
 app.use('/', express.static('frontend'))
  
+ 
+ 
 app.get('/hello', function (req, res) {
   res.send('Hello World')
+})
+
+
+
+app.get('/messages', function(request, response){
+  var messages = [] //aratam ca e un array cu []
+  var message={
+      name: "Antonia",
+      message:"Hello from the other side"
+  } //obiect in JS
+  messages.push(message)
+  response.status(200).send(messages);
+  
+  
+})
+
+app.post('/messages', function(request, response){
+  console.log(request.body)
+  response.send('Data: '+ request.body)
+  
 })
  
 app.listen(8080)
